35b113d119addf2b3e7f9218a5d415a8ff25a859
BonheurByiringiro <byibonheur@gmail.com>
2025-11-06 16:09:07 -0600

    Add placeholder files for AdminCoachList and AdminAthleteList to fix deployment

---
+++ src/views/AdminAthleteList.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-app-bar color="#800020" elevation="0" class="text-white">
+      <v-btn icon @click="$router.push({ name: 'admin-dashboard' })" class="text-white">
+        <v-icon color="white">mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-app-bar-title class="text-white">
+        <strong>OC</strong> Exercise Tracker - Athlete List
+      </v-app-bar-title>
+    </v-app-bar>
+
+    <v-container>
+      <v-row class="mt-5">
+        <v-col cols="12">
+          <h1>Athlete List</h1>
+          <v-alert type="info" class="mt-4">
+            This page is under construction. Coming soon!
+          </v-alert>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-container>
+</template>
+
+<script setup>
+// Placeholder component for Athlete List
+</script>
