0cb60333d4cea961aed9e07a18d05c864392e194
BonheurM <r.mfurankunda@eagles.oc.edu>
2025-12-04 00:09:38 -0600

    still debugging

--- src/services/coachServices.js
+++ src/services/coachServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Utils from "../config/utils.js";
 
-const BASE_URL = (import.meta.env.VITE_APP_BASE_URL || "http://localhost:3122") + "/tracker-t2";
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "http://localhost:3122";
 
 // Helper to get auth headers
 const getAuthHeaders = () => {
