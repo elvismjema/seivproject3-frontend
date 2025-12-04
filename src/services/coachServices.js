968453670b6ab76fbbf45ec7ef5b791e16ab3b0c
Michael Wittenberg <michael.wittenberg@eagles.oc.edu>
2025-12-04 11:58:03 -0600

    Fix duplicate BASE_URL declaration in coachServices

--- src/services/coachServices.js
+++ src/services/coachServices.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import Utils from "../config/utils.js";
 
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "http://localhost:3122";
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "https://project2.eaglesoftwareteam.com/tracker-t2";
 
 // Helper to get auth headers
