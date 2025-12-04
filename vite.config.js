b65596129ec246d4de75e0bf4ba1983dfa6da305
Michael Wittenberg <michael.wittenberg@eagles.oc.edu>
2025-12-04 11:52:09 -0600

    Add @ alias to vite config for import resolution

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
+import path from "path";
 
 import dns from "dns";
 dns.setDefaultResultOrder("verbatim");
@@ -11,6 +12,12 @@ export default () => {
 
   return defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
 
     server: {
       host: "localhost",
